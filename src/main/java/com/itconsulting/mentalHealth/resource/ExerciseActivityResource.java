2f1e128bada26e6878e64f8f5dec66e798cb455a
danqwas <daniel_inmaculada@hotmail.com>
2022-06-05 18:39:13 -0500

    update

---
+++ src/main/java/com/itconsulting/mentalHealth/resource/ExerciseActivityResource.java
@@ -0,0 +1,18 @@
+package com.itconsulting.mentalHealth.resource;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ExerciseActivityResource {
+    private Long id;
+    private String duration;
+
+    private String dayOfTheWeek;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
+    private Date exerciseDate;
+
+
+}
